13528f3d2f6297c9cdf46ef79f132f85683e15f4
yecanlin <yecanlin2@qq.com>
2018-07-07 17:46:13 +0800

    Get html file
    From web diretory read html file content

--- py3.py
+++ py3.py
@@ -1,5 +1,6 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import json
+from os import path
 
 data = {'result': 'this is a test'}
 
@@ -10,12 +11,16 @@ def run(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
 
 class myServer(BaseHTTPRequestHandler):
     def do_GET(self):
-        print(self.path)
-        self.send_response(200,"test")
-        self.send_header('Content-type','text/html')
-        self.end_headers()
-        # Send the html message
-        # self.wfile.write(json.dumps(data).encode())
-        self.wfile.write("<h1>hello world!</h1>".encode())
-        return
+        try:
+            with open(path.realpath("./web/index.html"), 'rb') as f:
+                content = f.read()
+                print(self.path)
+                self.send_response(200,"test")
+                self.send_header('Content-type','text/html')
+                self.end_headers()
+                # Send the html message
+                # self.wfile.write(json.dumps(data).encode())
+                self.wfile.write(content)
+        except  IOError:
+            self.send_error(404,'File Not Found')
 run(handler_class=myServer)
